a21c4fa469e6d7f498b12483a50f947f78a82bd1
Chicecream-nomeltechnology <azoth07@gmail.com>
2023-02-17 22:43:44 +0800

    Update Ashare.py

--- Ashare.py
+++ Ashare.py
@@ -9,8 +9,8 @@ def get_price_day_tx(code, end_date='', count=10, frequency='1d'):     #日线�
     URL=f'http://web.ifzq.gtimg.cn/appstock/app/fqkline/get?param={code},{unit},,{end_date},{count},qfq'     
     st= json.loads(requests.get(URL).content);    ms='qfq'+unit;      stk=st['data'][code]   
     buf=stk[ms] if ms in stk else stk[unit]       #指数返回不是qfqday,是day
-    df=pd.DataFrame(buf,columns=['time','open','close','high','low','volume'],dtype='float')     
-    df.time=pd.to_datetime(df.time);    df.set_index(['time'], inplace=True);   df.index.name=""      #处理索引 
+    df=pd.DataFrame(buf,columns=['day','open','close','high','low','volume'],dtype='float')     
+    #df.time=pd.to_datetime(df.time);    df.set_index(['time'], inplace=True);   df.index.name=""      #处理索引 
     return df
 
 #腾讯分钟线
@@ -19,10 +19,10 @@ def get_price_min_tx(code, end_date=None, count=10, frequency='1d'):    #分钟�
     if end_date: end_date=end_date.strftime('%Y-%m-%d') if isinstance(end_date,datetime.date) else end_date.split(' ')[0]        
     URL=f'http://ifzq.gtimg.cn/appstock/app/kline/mkline?param={code},m{ts},,{count}' 
     st= json.loads(requests.get(URL).content);       buf=st['data'][code]['m'+str(ts)] 
-    df=pd.DataFrame(buf,columns=['time','open','close','high','low','volume','n1','n2'])   
-    df=df[['time','open','close','high','low','volume']]    
-    df[['open','close','high','low','volume']]=df[['open','close','high','low','volume']].astype('float')
-    df.time=pd.to_datetime(df.time);   df.set_index(['time'], inplace=True);   df.index.name=''          #处理索引     
+    df=pd.DataFrame(buf,columns=['day','open','close','high','low','volume','n1','n2'])   
+    df=df[['day','open','close','high','low','volume']]    
+    df[['open','close','high','low','volume']]=df[['open','close','high','low','volume']].astype(float)
+    #df.time=pd.to_datetime(df.time);   df.set_index(['time'], inplace=True);   df.index.name=''          #处理索引     
     df['close'][-1]=float(st['data'][code]['qt'][code][3])                #最新基金数据是3位的
     return df
 
